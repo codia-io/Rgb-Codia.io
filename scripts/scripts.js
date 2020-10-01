a9d434da76e7db88cbc0ea3223be04d44cb32672
username <email>
2020-10-01 19:49:59 +0100

    header animation improvement

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,8 +1,16 @@
 $(window).scroll(function () {
   var scroll = $(window).scrollTop();
   if (scroll < 200) {
+    // original scroll place
     $("header").removeClass("scroll-effect-on");
+    $("#img-logo").removeClass("img-logo-on");
+    $("#navBar").addClass("mt-2");
+    $("header").removeClass("border-bot");
   } else {
+    //when scrolled down
     $("header").addClass("scroll-effect-on");
+    $("#img-logo").addClass("img-logo-on");
+    $("#navBar").removeClass("mt-2");
+    $("header").addClass("border-bot");
   }
 });
