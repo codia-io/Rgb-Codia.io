4c7f3f733aa41179fa521a3f9f106981a407d87a
username <email>
2020-10-02 12:20:34 +0100

    final commit

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,14 +3,14 @@ $(window).scroll(function () {
   if (scroll < 200) {
     // original scroll place
     $("header").removeClass("scroll-effect-on");
-    $("#img-logo").removeClass("img-logo-on");
-    $("#navBar").addClass("mt-2");
+    $("#img-logo").removeClass("img-logo-on mt-1");
+    $("header>div").addClass("mt-3");
     $("header").removeClass("border-bot");
   } else {
     //when scrolled down
     $("header").addClass("scroll-effect-on");
-    $("#img-logo").addClass("img-logo-on");
-    $("#navBar").removeClass("mt-2");
+    $("#img-logo").addClass("img-logo-on mt-1");
+    $("header>div").removeClass("mt-3");
     $("header").addClass("border-bot");
   }
 });
