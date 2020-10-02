49aba82cd2f8a46c2899ebd1a2d148735e6ac54a
username <email>
2020-10-02 13:21:28 +0100

    added text fade out

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,21 @@
+// navbar and showcase scroll effect
+const checkpoint = 200;
 $(window).scroll(function () {
   var scroll = $(window).scrollTop();
-  if (scroll < 200) {
+  if (scroll < checkpoint) {
     // original scroll place
     $("header").removeClass("scroll-effect-on");
     $("#img-logo").removeClass("img-logo-on mt-1");
     $("header>div").addClass("mt-3");
     $("header").removeClass("border-bot");
+    opacity = 1 - scroll / checkpoint;
   } else {
     //when scrolled down
     $("header").addClass("scroll-effect-on");
     $("#img-logo").addClass("img-logo-on mt-1");
     $("header>div").removeClass("mt-3");
     $("header").addClass("border-bot");
+    opacity = 0;
   }
+  document.querySelector("#main-text").style.opacity = opacity;
 });
