3641255ea9fd9448feb17d6bbb8975c359fc071a
username <email>
2020-10-05 18:46:54 +0100

    added our creations

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,21 +1,24 @@
 // navbar and showcase scroll effect
 const checkpoint = 200;
-$(window).scroll(function () {
-  var scroll = $(window).scrollTop();
-  if (scroll < checkpoint) {
-    // original scroll place
-    $("header").removeClass("scroll-effect-on");
-    $("#img-logo").removeClass("img-logo-on mt-1");
-    $("header>div").addClass("mt-3");
-    $("header").removeClass("border-bot");
-    opacity = 1 - scroll / checkpoint;
-  } else {
-    //when scrolled down
-    $("header").addClass("scroll-effect-on");
-    $("#img-logo").addClass("img-logo-on mt-1");
-    $("header>div").removeClass("mt-3");
-    $("header").addClass("border-bot");
-    opacity = 0;
-  }
-  document.querySelector("#main-text").style.opacity = opacity;
-});
+
+function menu_animation() {
+    var scroll = $(window).scrollTop();
+    if (scroll < checkpoint) {
+        // original scroll place
+        $("header").removeClass("scroll-effect-on");
+        $("#img-logo").removeClass("img-logo-on mt-1");
+        $("header>div").addClass("mt-3");
+        $("header").removeClass("border-bot");
+        opacity = 1 - scroll / checkpoint;
+    } else {
+        //when scrolled down
+        $("header").addClass("scroll-effect-on");
+        $("#img-logo").addClass("img-logo-on mt-1");
+        $("header>div").removeClass("mt-3");
+        $("header").addClass("border-bot");
+        opacity = 0;
+    }
+    document.querySelector("#main-text").style.opacity = opacity;
+}
+$(window).scroll(menu_animation);
+menu_animation()
